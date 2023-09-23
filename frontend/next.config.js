9afcb2887dbdeee3dfeae0e62bfa3053d7f80f6a
Arya <arya.vaziri@gmail.com>
2023-09-23 18:22:05 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   basePath: "/my-personal-next",
-  assetPrefix: "/my-personal-next/",
+  assetPrefix: isProd
+    ? '"https://github.com/aryavaziri/my-personal-next/"'
+    : undefined,
   images: {
     remotePatterns: [
       {
